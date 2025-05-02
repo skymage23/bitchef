f6a1317bd8964ad5893ab3a68df7da2213012ccd
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-05-02 15:34:10 -0500

    Updated dependencies. Began work on comms library. Began work on logging library. Began work on shared exceptions library.

--- bitchef/utilities/communications_source/src/communications.cc
+++
@@ -1,9 +0,0 @@
-/*This is where we actually define the communications Python
-module*/
-#include <python3.12/Python.h>
-#include "__communications.hpp"
-
-//Remember. What is defined here are PYTHON OBJECTS.
-//You need to decide what the Python interface
-//for this class will look like.
-
